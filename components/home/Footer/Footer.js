a231fcd8202ed84ac2d4dcfabaeba40b345b2e40
Irvin-Kier <irvinkierpresto26@gmail.com>
2023-07-09 02:51:43 +0800

    Updated Footer.js contest

--- components/home/Footer/Footer.js
+++ components/home/Footer/Footer.js
@@ -7,26 +7,29 @@ const Footer = () => {
   return (
     <footer className={classes.container}>
       <div className={`  ${classes["footer-top"]}`}>
-        <div className={classes["footer_description"]}>
-          <h6>Quick links</h6>
-          <div>Lorem ipsum dolor</div>
-          <div>Lorem ipsum dolor</div>
-          <div>Lorem ipsum dolor</div>
-          <div>Lorem ipsum </div>
+      <div className={classes["footer_description"]}>
+          <h6>SUDS & SCENTS</h6>
+          <div>Suds & Scents is a brand that specializes in personalized hygiene products.<br></br> We offer a wide range of products all tailored to meet the unique needs and<br></br> preferences of our customers.</div>
         </div>
         <div className={classes["footer_description"]}>
-          <h6>Quick links</h6>
-          <div>Lorem ipsum dolor</div>
-          <div>Lorem ipsum dolor</div>
-          <div>Lorem ipsum dolor</div>
-          <div>Lorem ipsum </div>
+          <h6>PRODUCTS</h6>
+          <div>Perfume</div>
+          <div>Body Wash</div>
+          <div>Shampoo</div>
+          <div>Dishwashing Liquid </div>
+          <div>Liquid Detergent</div>
         </div>
         <div className={classes["footer_description"]}>
-          <h6>Quick links</h6>
-          <div>Lorem ipsum dolor</div>
-          <div>Lorem ipsum dolor</div>
-          <div>Lorem ipsum dolor</div>
-          <div>Lorem ipsum </div>
+          <h6>SOCIALS</h6>
+          <div>Instagram</div>
+          <div>Facebook</div>
+          <div>Twitter</div>
+        </div>
+        <div className={classes["footer_description"]}>
+          <h6>OTHERS</h6>
+          <div>Terms and Conditions</div>
+          <div>Privacy Policy</div>
+          <div>Help Center</div>
         </div>
       </div>
       <Divider />
